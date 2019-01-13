07e33b04adf8be81b5c5b39eb08d0d67782a2a9b
Paellis12 <patrick.ellis84@yahoo.com>
2019-01-13 12:35:14 -0500

    made edits in html and js files to clean up

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,14 +1,9 @@
-//We have to create the random "number to guess" by grabbing the id from html and implementing it here.//
 $( document ).ready(function(){
-    var randomNumber = Math.floor(Math.random()*103+17)  //Creates the random number (hopefully) in between 17-120 to be shown upon start of the game.//
-       
+    //Creates the random number (hopefully) in between 17-120 to be shown upon start of the game.//
+    var randomNumber = Math.floor(Math.random()*103+17)  
+     
+    //We have to create the random "number to guess" by grabbing the id from html and implementing it here.//  
     $("#randomNum").text(randomNumber); //This is how the random number will show up on the HTML //
-
-    var jewel1 = Math.floor(Math.random()*15+1)
-    var jewel2 = Math.floor(Math.random()*15+1)
-    var jewel3 = Math.floor(Math.random()*15+1)
-    var jewel4 = Math.floor(Math.random()*15+1) //Giving random numbers to each "crystal" that should be in between 1-16//
-
     var wins = 0;
     var losses = 0;
     var total = 0; //Keeping track of the users Wins, Losses, and the Total Score. //
@@ -16,8 +11,16 @@ $( document ).ready(function(){
     $("#winNum").text(wins);
     $("#lossNum").text(losses); //applies the number of wins and losses to the HTML//
 
+    
+    var jewel1 = Math.floor(Math.random()*15+1)
+    var jewel2 = Math.floor(Math.random()*15+1)
+    var jewel3 = Math.floor(Math.random()*15+1)
+    var jewel4 = Math.floor(Math.random()*15+1) //Giving random numbers to each "crystal" that should be in between 1-16//
+
+    
+
     //Now we will set up a reset function that will "reset" the users score when a "win" or "loss" is found.//
-      //We will also console.log the random number being generated for checking purposes//
+    //We will also console.log the random number being generated for checking purposes//
       function reset() {
         randomNumber = Math.floor(Math.random()*103+17);
         console.log(randomNumber);
@@ -26,7 +29,7 @@ $( document ).ready(function(){
          jewel2 = Math.floor(Math.random()*15+1)
          jewel3 = Math.floor(Math.random()*15+1)
          jewel4 = Math.floor(Math.random()*15+1)
-        total = 0;
+         total = 0;
         $('#userTotal').text(total);
 
       }
@@ -45,17 +48,17 @@ $( document ).ready(function(){
         alert("YOU LOSE!");
         losses = losses + 1;
         $("#lossNum").text(losses);
-        reset()
+        reset();
     }
 
     //Now I want to give the crystals functionality//
     //Start with an on click function for each individual crystal and set values accordingly //
     //clicks will also console.log the outcome of each crystal.
 
-    $('#crystal1').on ("click", function(){
+    $("#crystal1").on ("click", function(){
         total = total + jewel1;
         console.log("New userTotal= " + total);
-        $('#userTotal').text(total); 
+        $("#userTotal").text(total); 
               
             if (total === randomNumber){
               goodJob();
@@ -65,10 +68,10 @@ $( document ).ready(function(){
             }   
       })
 
-      $('#diamond').on ("click", function(){
+      $("#diamond").on ("click", function(){
         total = total + jewel2;
         console.log("New userTotal= " + total);
-        $('#userTotal').text(total); 
+        $("#userTotal").text(total); 
               
             if (total === randomNumber){
               goodJob();
@@ -78,10 +81,10 @@ $( document ).ready(function(){
             }   
       })
 
-      $('#gemstone').on ("click", function(){
+      $("#gemstone").on ("click", function(){
         total = total + jewel3;
         console.log("New userTotal= " + total);
-        $('#userTotal').text(total); 
+        $("#userTotal").text(total); 
               
             if (total === randomNumber){
               goodJob();
@@ -91,10 +94,10 @@ $( document ).ready(function(){
             }   
       })
 
-      $('#diamond1').on ("click", function(){
+      $("#diamond1").on ("click", function(){
         total = total + jewel4;
         console.log("New userTotal= " + total);
-        $('#userTotal').text(total); 
+        $("#userTotal").text(total); 
               
             if (total === randomNumber){
               goodJob();
